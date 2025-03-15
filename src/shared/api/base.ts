88af7a3f0ebc386a32e869157fc12b133ae9e62f
2Revol2 <maksimbaluta02@gmail.com>
2025-03-15 11:35:22 +0300

    fix: fix bug

--- src/shared/api/base.ts
+++ src/shared/api/base.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 function createInstance() {
   const instance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_IRL,
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
   });
   return instance;
 }
